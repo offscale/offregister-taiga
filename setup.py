43a47ac1a7287b9ef0c500d8f0a2948994b8c075
Samuel Marks <samuelmarks@gmail.com>
2016-11-07 21:46:08 +1100

    Updating

--- setup.py
+++ setup.py
@@ -3,22 +3,21 @@ from os import path, listdir
 from functools import partial
 from itertools import imap, ifilter
 from ast import parse
-from pip import __file__ as pip_loc
+from distutils.sysconfig import get_python_lib
 
 if __name__ == '__main__':
     package_name = 'offregister_taiga'
 
-    f_for = partial(path.join, path.dirname(__file__), package_name)
-    d_for = partial(path.join, path.dirname(path.dirname(pip_loc)), package_name)
-
-    _data_join = partial(path.join, f_for('data'))
-    _data_install_dir = partial(path.join, d_for('data'))
-
-    get_vals = lambda var0, var1: imap(lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-                                       ifilter(lambda line: line.startswith(var0) or line.startswith(var1), f))
-
     with open(path.join(package_name, '__init__.py')) as f:
-        __author__, __version__ = get_vals('__version__', '__author__')
+        __author__, __version__ = imap(
+            lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
+            ifilter(lambda line: line.startswith('__version__') or line.startswith('__author__'), f)
+        )
+
+    to_funcs = lambda *paths: (partial(path.join, path.dirname(__file__), package_name, *paths),
+                               partial(path.join, get_python_lib(prefix=''), package_name, *paths))
+
+    data_join, data_install_dir = to_funcs('data')
 
     setup(
         name=package_name,
@@ -27,7 +26,8 @@ if __name__ == '__main__':
         test_suite=package_name + '.tests',
         packages=find_packages(),
         package_dir={package_name: package_name},
+        install_requires=['fabric'],
         data_files=[
-            (_data_install_dir(), map(_data_join, listdir(_data_join()))),
+            (data_install_dir(), map(data_join, listdir(data_join())))
         ]
     )
